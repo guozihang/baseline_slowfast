48d1a45021abf04397b11445ac44d35d309ece72
子航 <15047271937@163.com>
2024-09-23 07:36:39 +0800

    feat: add reproduct run file.

---
+++ run_ph_offical_reproduct.sh
@@ -0,0 +1,1 @@
+python main.py --device 3 --dataset phoenix2014 --loss-weights Slow=0.25 Fast=0.25 --work-dir /share/huaiwen_group/guozihang/work_dir/slowfast_offical_reproduct_ph
---
+++ run_pht_offical_reproduct.sh
@@ -0,0 +1,1 @@
+python main.py --device 1 --dataset phoenix2014-T --loss-weights Slow=0.25 Fast=0.25 --work-dir /share/huaiwen_group/guozihang/work_dir/slowfast_offical_reproduct_pht
